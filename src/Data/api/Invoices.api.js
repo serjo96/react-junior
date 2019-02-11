287fe367bd98384505567e20b5fea7fc73f5f561
Serjo96 <king-beer@yandex.ru>
2019-02-11 13:27:41 +0500

    Cleanup code

--- src/Data/api/Invoices.api.js
+++ src/Data/api/Invoices.api.js
@@ -22,22 +22,7 @@ export function onLoadInvoicesEditPage(id) {
                 dispatch(totalCalculation());
             }))
             .catch(error => {
-                if (error.response) {
-                    // The request was made and the server responded with a status code
-                    // that falls out of the range of 2xx
-                    console.log(error.response.data);
-                    console.log(error.response.status);
-                    console.log(error.response.headers);
-
-                } else if (error.request) {
-                    // The request was made but no response was received
-                    // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-                    // http.ClientRequest in node.js
-                    console.log(error.request);
-                } else {
-                    // Something happened in setting up the request that triggered an Error
-                    console.log('Error', error.message);
-                }
+                dispatch(onResponseErrorRequest(error));
             });
     };
 }
@@ -58,7 +43,7 @@ export function onLoadInvoices() {
                 }));
             }))
             .catch(error => {
-                console.error(error);
+                dispatch(onResponseErrorRequest(error));
             });
     };
 }
